d04ab242802ea46febb977d28a5e7a606da1dd1c
Anna Beatriz Marques da Silva <annabiamques089@gmail.com>
2023-08-25 12:03:38 -0300

    Criação do ambiente de desnevolvimento

---
+++ src/test/java/com/itb/tcc/lojadetatuagem/LojaDeTatuagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.lojadetatuagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeTatuagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
